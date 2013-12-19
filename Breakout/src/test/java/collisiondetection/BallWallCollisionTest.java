e708709214fd3de2279bb0b2d791116e90797d38
Joel Järvinen <joel.jarvinen@cs.helsinki.fi>
2013-12-19 15:25:21 +0200

    korjauksia

--- Breakout/src/test/java/collisiondetection/BallWallCollisionTest.java
+++ Breakout/src/test/java/collisiondetection/BallWallCollisionTest.java
@@ -5,11 +5,7 @@
 package collisiondetection;
 
 import entities.Player;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import sprites.Ball;
 import sprites.Paddle;
@@ -34,22 +30,6 @@ public class BallWallCollisionTest {
         player = new Player(1);
         ball.launchBall();
         ball.setPaddle(paddle);
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
     }
 
     @Test
