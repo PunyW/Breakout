83a905f543dd5f9d78bde35c2d69d70db4035f85
jajojajo <joel.jarvinen@cs.helsinki.fi>
2013-12-26 18:13:34 +0200

    korjauksia

--- Breakout/src/main/java/collisiondetection/BallBricksCollision.java
+++ Breakout/src/main/java/collisiondetection/BallBricksCollision.java
@@ -1,6 +1,7 @@
 package collisiondetection;
 
 import entities.Player;
+import java.awt.Point;
 import java.awt.Rectangle;
 import sprites.Ball;
 import sprites.Brick;
@@ -26,15 +27,38 @@ public class BallBricksCollision {
         Rectangle ballRect = ball.getCollisionRect();
         Rectangle brickRect = brick.getCollisionRect();
 
+        Point ballRight = new Point(ballRect.x + ballRect.width + 1, ballRect.y);
+        Point ballLeft = new Point(ballRect.x - 1, ballRect.y);
+        Point ballTop = new Point(ballRect.x, ballRect.y - 1);
+        Point ballBottom = new Point(ballRect.x, ballRect.y + ballRect.height + 1);
+
         if (ballRect.intersects(brickRect)) {
-            if (ballRect.getX() == brickRect.getX()) {
-                ball.reverseHorizontalMomentum();
-            } else {
+
+            if (brickRect.contains(ballRight)) {
+                ball.reverseVerticalMomentum();
+            }
+            if (brickRect.contains(ballLeft)) {
+                ball.reverseVerticalMomentum();
+            }
+            if (brickRect.contains(ballTop)) {
+                ball.reverseVerticalMomentum();
+            }
+            if (brickRect.contains(ballBottom)) {
                 ball.reverseVerticalMomentum();
             }
             brick.hit();
             player.increaseScore(10);
         }
 
+//        if (ballRect.intersects(brickRect)) {
+//            if (ballRect.y == brickRect.y + brickRect.height) {
+//                ball.reverseVerticalMomentum();
+//            } else if (ballRect.getY() + ballRect.height == brickRect.getY()) {
+//                ball.reverseVerticalMomentum();
+//            }
+//            brick.hit();
+//            player.increaseScore(10);
+//        }
     }
+
 }
