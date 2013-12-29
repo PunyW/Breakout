3bdd8dc68c8754dcfdbaea73baffb70c0f2f9c64
jajojajo <joel.jarvinen@cs.helsinki.fi>
2013-12-29 19:04:56 +0200

    Killing them mutants

--- Breakout/src/main/java/collisiondetection/BallPaddleCollision.java
+++ Breakout/src/main/java/collisiondetection/BallPaddleCollision.java
@@ -23,12 +23,11 @@ public class BallPaddleCollision {
     /**
      * Checks if the ball is colliding with the paddle.
      * <p>
-     * If the collision is on top of the paddle reverse horizontal momentum, 
-     * and calculate where the hit was after which change vertical momentum
-     * accordingly.
-     * 
-     * @param ball  Ball which is being inspected if it collides with the paddle
-     * 
+     * If the collision is on top of the paddle reverse horizontal momentum, and
+     * check which "zone" was hit and change the vertical momentum accordingly
+     *
+     * @param ball Ball which is being inspected if it collides with the paddle
+     *
      */
     public void collides(Ball ball) {
         Rectangle rect1 = ball.getCollisionRect();
@@ -37,8 +36,27 @@ public class BallPaddleCollision {
         if (rect1.intersects(rect2)) {
             if (ball.getY() < paddle.getY()) {
                 ball.reverseVerticalMomentum();
-                int direction = paddle.getCenter() - ball.getX();
-                ball.setDx(-direction);
+
+                /*
+                 * Zone One is the leftmost part of the paddle, zone two 
+                 * is the middle left part of the paddle, zone three middle right
+                 * and lastly the rightmost part of the paddle
+                 */
+                int zoneWidth = paddle.getWidth() / 4;
+                int zoneOne = paddle.getX() + zoneWidth;
+                int zoneTwo = zoneOne + zoneWidth;
+                int zoneThree = zoneTwo + zoneWidth;
+
+                if (ball.getX() < zoneOne) {
+                    ball.setDx(-8);
+                } else if (ball.getX() < zoneTwo) {
+                    ball.setDx(-5);
+                } else if (ball.getX() < zoneThree) {
+                    ball.setDx(5);
+                } else {
+                    ball.setDx(8);
+                }
+
             } else {
                 ball.reverseHorizontalMomentum();
             }
---
+++ Breakout/src/test/java/collisiondetection/BallPaddleCollisionTest.java
@@ -0,0 +1,85 @@
+package collisiondetection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import sprites.Ball;
+import sprites.Paddle;
+
+/**
+ *
+ * @author Joel
+ */
+public class BallPaddleCollisionTest {
+
+    private final Paddle paddle;
+    private Ball ball;
+    private BallPaddleCollision cd;
+
+    public BallPaddleCollisionTest() {
+        paddle = new Paddle(30, 175, 20, 60, 200);
+        cd = new BallPaddleCollision(paddle);
+    }
+
+    /*
+     * Zones to be examined 
+     * Zone 1    30 - 44, dX set to -8
+     * Zone 2    45 - 59, dX set to -5
+     * Zone 3    60 - 74, dX set to 5
+     * Zone 4    75 - 90, dX set to 8
+     */
+    @Test
+    public void collisionInZoneOne() {
+        ball = new Ball(35, 161, 15, 15);
+        move();
+        test(-8, -5, 27, 156);
+    }
+
+    @Test
+    public void collisionInZoneTwo() {
+        ball = new Ball(45, 161, 15, 15);
+        move();
+        test(-5, -5, 40, 156);
+    }
+
+    @Test
+    public void collisionInZoneThree() {
+        ball = new Ball(65, 161, 15, 15);
+        // KILL THE MUTANT
+        ball.setDx(-4);
+        move();
+        test(5, -5, 70, 156);
+    }
+
+    @Test
+    public void collisionInZoneFour() {
+        ball = new Ball(75, 161, 15, 15);
+        move();
+        test(8, -5, 83, 156);
+    }
+    
+    @Test
+    public void collisionOnSide() {
+        ball = new Ball(89, 185, 15, 15);
+        ball.setDx(-5);
+        move();
+        test(5, 5, 94, 190);
+    }
+
+    private void move() {
+        ball.setDy(5);
+        ball.launchBall();
+        cd.collides(ball);
+        ball.move();
+    }
+
+    private void test(int dX, int dY, int x, int y) {
+        assertEquals(dX, ball.getDx());
+        assertEquals(dY, ball.getDy());
+        assertEquals(x, ball.getX());
+        assertEquals(y, ball.getY());
+    }
+}
